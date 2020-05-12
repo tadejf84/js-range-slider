9ba4f6dee7c57405df5bc96d7b70933222a02416
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-05-12 17:32:54 +0200

    app logic done

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
 class Slider {
 
     constructor({ DOMselector, sliders }) {
-
         this.DOMselector = DOMselector;
-        this.container = document.querySelector(this.DOMselector);  // slider container
-        this.sliderWidth = 400;                                     // slider width
-        this.sliderHeight = 400;                                    // slider length
-        this.cx = this.sliderWidth / 2;                             // slider center X coordinate
-        this.cy = this.sliderHeight / 2;                            // slider center Y coordinate
+        this.container = document.querySelector(this.DOMselector);  // Slider container
+        this.sliderWidth = 400;                                     // Slider width
+        this.sliderHeight = 400;                                    // Slider length
+        this.cx = this.sliderWidth / 2;                             // Slider center X coordinate
+        this.cy = this.sliderHeight / 2;                            // Slider center Y coordinate
         this.tau = 2 * Math.PI;                                     // Tau constant
-        this.sliders = sliders;                                     // sliders array with opts for each slider
-        this.arcFractionSpacing = 0.85;                             // spacing between arc fractions
-        this.arcFractionLength = 10;
-        this.arcFractionThickness = 25;
-        this.arcBgFractionColor = '#D8D8D8';
-
-        // handle opts
-        this.handleFillColor = '#fff';
-        this.handleStrokeColor = '#888888';
-        this.handleStrokeThickness = 3;
-
-        this.mouseDown = false;
-        this.activeSlider = null;
+        this.sliders = sliders;                                     // Sliders array with opts for each slider
+        this.arcFractionSpacing = 0.85;                             // Spacing between arc fractions
+        this.arcFractionLength = 10;                                // Arc fraction length
+        this.arcFractionThickness = 25;                             // Arc fraction thickness
+        this.arcBgFractionColor = '#D8D8D8';                        // Arc fraction color for background slider
+        this.handleFillColor = '#fff';                              // Slider handle fill color
+        this.handleStrokeColor = '#888888';                         // Slider handle stroke color
+        this.handleStrokeThickness = 3;                             // Slider handle stroke thickness    
+        this.mouseDown = false;                                     // Is mouse down
+        this.activeSlider = null;                                   // Stores active (selected) slider
     }
 
 
@@ -31,21 +27,20 @@ class Slider {
      */
     draw() {
 
-        // Update legend UI
-        this.outputLegendUI();
+        // Create legend UI
+        this.createLegendUI();
 
-        // Create and append SVG holder first
+        // Create and append SVG holder
         const svgContainer = document.createElement('div');
         svgContainer.classList.add('slider__data');
         const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-        svg.setAttribute('id', 'sliderSvg');
         svg.setAttribute('height', this.sliderWidth);
         svg.setAttribute('width', this.sliderHeight);
         svgContainer.appendChild(svg);
         this.container.appendChild(svgContainer);
 
         // Draw sliders
-        this.sliders.forEach( (slider, index) => this.drawSingleSlider(svg, slider, index) );
+        this.sliders.forEach( (slider, index) => this.drawSingleSliderOnInit(svg, slider, index) );
 
         // Event listeners
         svgContainer.addEventListener('mousedown', this.mouseTouchStart.bind(this), false);
@@ -58,26 +53,24 @@ class Slider {
 
 
     /**
-     * Draw single slider
-     *
-     */
-    drawSingleSlider(svg, slider, index) {
-        this.outputSliderSVG (svg, slider, index);
-    }
-
-    /**
-     * Generate SVG markup for slider
+     * Draw single slider on init
      * 
+     * @param {object} svg 
+     * @param {object} slider 
+     * @param {number} index 
      */
-    outputSliderSVG (svg, slider, index) {
+    drawSingleSliderOnInit(svg, slider, index) {
 
+        // Calculate slider circumference
         const circumference = slider.radius * this.tau;
+
+        // Calculate initial angle
         const initialAngle = Math.floor( (slider.initialValue / (slider.max - slider.min)) * 360 );
 
         // Calculate spacing between arc fractions
         const arcFractionSpacing = this.calculateSpacingBetweenArcFractions(circumference, this.arcFractionLength, this.arcFractionSpacing);
 
-        // Create a single slider group - holds background and active paths
+        // Create a single slider group - holds all paths and handle
         const sliderGroup = document.createElementNS('http://www.w3.org/2000/svg', 'g');
         sliderGroup.setAttribute('class', 'sliderSingle');
         sliderGroup.setAttribute('data-slider', index);
@@ -85,35 +78,15 @@ class Slider {
         sliderGroup.setAttribute('rad', slider.radius);
         svg.appendChild(sliderGroup);
         
-        // Create single slider background
-        this.outputArcPath( this.arcBgFractionColor, slider.radius, 360, arcFractionSpacing, 'bg', sliderGroup);
+        // Draw background arc path
+        this.drawArcPath(this.arcBgFractionColor, slider.radius, 360, arcFractionSpacing, 'bg', sliderGroup);
 
-        // Create background for single slider
-        this.outputArcPath( slider.color, slider.radius, initialAngle, arcFractionSpacing, 'active', sliderGroup);
+        // Draw active arc path
+        this.drawArcPath(slider.color, slider.radius, initialAngle, arcFractionSpacing, 'active', sliderGroup);
 
         // Draw handle
-        this.drawHandle(slider, initialAngle, sliderGroup );
+        this.drawHandle(slider, initialAngle, sliderGroup);
     }
-
-
-    /**
-     * Draw handle for single slider
-     * 
-     * @param {object} slider 
-     */
-    drawHandle(slider, initialAngle, group) {
-        const handle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-        const handleCenter = this.calculateHandleCenter( initialAngle * this.tau / 360, slider.radius);
-        handle.setAttribute('class', 'sliderHandle');
-        handle.setAttribute('cx', handleCenter.x);
-        handle.setAttribute('cy', handleCenter.y);
-        handle.setAttribute('r', this.arcFractionThickness / 2);
-        handle.style.stroke = this.handleStrokeColor;
-        handle.style.strokeWidth = this.handleStrokeThickness;
-        handle.style.fill = this.handleFillColor;
-        group.appendChild(handle);
-    }
-
 
 
     /**
@@ -126,7 +99,7 @@ class Slider {
      * @param {number} singleSpacing 
      * @param {string} type 
      */
-    outputArcPath( color, radius, angle, singleSpacing, type, group ) {
+    drawArcPath( color, radius, angle, singleSpacing, type, group ) {
 
         // Slider path class
         const pathClass = (type === 'active') ? 'sliderSinglePathActive' : 'sliderSinglePath';
@@ -139,21 +112,52 @@ class Slider {
         path.style.strokeWidth = this.arcFractionThickness;
         path.style.fill = 'none';
         path.setAttribute('stroke-dasharray', this.arcFractionLength + ' ' + singleSpacing);
-
-        // Append path to svg group
         group.appendChild(path);
     }
 
-    
-    outputLegendUI() {
+
+    /**
+     * Draw handle for single slider
+     * 
+     * @param {object} slider 
+     * @param {number} initialAngle 
+     * @param {group} group 
+     */
+    drawHandle(slider, initialAngle, group) {
+
+        // Calculate handle center
+        const handleCenter = this.calculateHandleCenter(initialAngle * this.tau / 360, slider.radius);
+
+        // Draw handle
+        const handle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+        handle.setAttribute('class', 'sliderHandle');
+        handle.setAttribute('cx', handleCenter.x);
+        handle.setAttribute('cy', handleCenter.y);
+        handle.setAttribute('r', this.arcFractionThickness / 2);
+        handle.style.stroke = this.handleStrokeColor;
+        handle.style.strokeWidth = this.handleStrokeThickness;
+        handle.style.fill = this.handleFillColor;
+        group.appendChild(handle);
+    }
+
+
+    /**
+     * Create legend UI on init
+     * 
+     */
+    createLegendUI() {
+
+        // Create legend
         const display = document.createElement('ul');
-        display.setAttribute('id', 'displayValues');
         display.classList.add('slider__legend');
+
+        // Legend heading
         const heading = document.createElement('h2');
         heading.innerText = 'Legend';
+        display.appendChild(heading);
 
-        // Create legend for all sliders
-        this.sliders.forEach( (slider, index) => {
+        // Legend data for all sliders
+        this.sliders.forEach((slider, index) => {
             const li = document.createElement('li');
             li.setAttribute('data-slider', index);
             const firstSpan = document.createElement('span');
@@ -171,7 +175,6 @@ class Slider {
         });
 
         // Append to DOM
-        display.appendChild(heading);
         this.container.appendChild(display);
     }
 
@@ -219,34 +222,42 @@ class Slider {
 
 
     /**
-     * Mouse touch start event
+     * Mouse down / Touch start event
      * 
      * @param {object} e 
      */
-    mouseTouchStart (e) {
+    mouseTouchStart(e) {
         if (this.mouseDown) return;
         this.mouseDown = true;
         const rmc = this.getRelativeMouseCoordinates(e);
-        const closestSlider = this.findClosestSlider(rmc);
+        this.findClosestSlider(rmc);
         this.redrawActiveSlider(rmc);
     }
 
-    // get new values for active slider on mouse move or touch move event
+
+    /**
+     * Mouse move / touch move event
+     * 
+     * @param {object} e 
+     */
     mouseTouchMove(e) {
         if (!this.mouseDown) return;
         e.preventDefault();
         const rmc = this.getRelativeMouseCoordinates(e);
         this.redrawActiveSlider(rmc);
-      }
+    }
 
 
-      // deactivate slider on mouse up or touch end event
+    /**
+     * Mouse move / touch move event
+     * Deactivate slider
+     * 
+     */
     mouseTouchEnd() {
         if (!this.mouseDown) return;
         this.mouseDown = false;
         this.activeSlider = null;
-        // firstSpan.removeAttribute('class');
-      }
+    }
 
 
     /**
@@ -256,7 +267,7 @@ class Slider {
      * @param {number} arcBgFractionLength 
      * @param {number} arcBgFractionBetweenSpacing 
      * 
-     * @return {number} arcFractionSpacing
+     * @returns {number} arcFractionSpacing
      */
     calculateSpacingBetweenArcFractions(circumference, arcBgFractionLength, arcBgFractionBetweenSpacing) {
         const numFractions = Math.floor((circumference / arcBgFractionLength) * arcBgFractionBetweenSpacing);
@@ -266,13 +277,15 @@ class Slider {
   
 
     /**
-     * Describe arc helper functions
+     * Helper functiom - describe arc
      * 
      * @param {number} x 
      * @param {number} y 
      * @param {number} radius 
      * @param {number} startAngle 
      * @param {number} endAngle 
+     * 
+     * @returns {string} path
      */
     describeArc (x, y, radius, startAngle, endAngle) {
         let endAngleOriginal, start, end, arcSweep, path;
@@ -309,39 +322,40 @@ class Slider {
      * @param {number} centerY 
      * @param {number} radius 
      * @param {number} angleInDegrees 
+     * 
+     * @returns {object} coords
      */
-     // function polar to cartesian transformation
      polarToCartesian (centerX, centerY, radius, angleInDegrees) {
-        let angleInRadians = angleInDegrees * Math.PI / 180;
-        return {
-            x: centerX + (radius * Math.cos(angleInRadians)),
-            y: centerY + (radius * Math.sin(angleInRadians))
-        };
+        const angleInRadians = angleInDegrees * Math.PI / 180;
+        const x = centerX + (radius * Math.cos(angleInRadians));
+        const y = centerY + (radius * Math.sin(angleInRadians));
+        return {x, y};
     }
 
 
     /**
-     * Calculate handle center
+     * Helper function - calculate handle center
      * 
      * @param {number} angle 
-     * @param {number} radius 
+     * @param {number} radius
+     * 
+     * @returns {object} coords 
      */
     calculateHandleCenter (angle, radius) {
         const x = this.cx + Math.cos(angle) * radius;
         const y = this.cy + Math.sin(angle) * radius;
-        return {
-            'x': x,
-            'y': y
-        };
+        return {x, y};
     }
 
 
     /**
      * Get mouse coordinates relative to the top and left of the container
      *  
-     * @param {string} e
+     * @param {object} e
+     * 
+     * @returns {object} coords
      */ 
-    getRelativeMouseCoordinates (e) {
+    getRelativeMouseCoordinates (e) {   
         const containerRect = document.querySelector('.slider__data').getBoundingClientRect();
         const x = e.clientX - containerRect.left;
         const y = e.clientY - containerRect.top;
@@ -353,6 +367,8 @@ class Slider {
      * Calculate mouse angle in radians
      * 
      * @param {object} rmc 
+     * 
+     * @returns {number} angle
      */
     calculateMouseAngle(rmc) {
         const angle = Math.atan2(rmc.y - this.cy, rmc.x - this.cx);
@@ -365,9 +381,11 @@ class Slider {
 
 
     /**
-     * Transform radians to degrees
+     * Helper function - transform radians to degrees
      * 
      * @param {number} angle 
+     * 
+     * @returns {number} angle
      */
     radiansToDegrees(angle) {
         return angle / (Math.PI / 180);
@@ -375,9 +393,10 @@ class Slider {
 
 
     /**
+     * Find closest slider to mouse pointer
+     * Activate the slider
      * 
-     * @param {object} e 
-     * @param {array} sliders 
+     * @param {object} rmc
      */
     findClosestSlider(rmc) {
         const mouseDistanceFromCenter = Math.hypot(rmc.x - this.cx, rmc.y - this.cy);
